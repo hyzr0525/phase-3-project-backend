f51b99d3bcead32eb4882a5dfa079cf4906adfe4
Yewei <yeweichen1@gmail.com>
2021-10-27 14:46:05 -0500

    add HeroClass Cards

--- app/controllers/hero_controller.rb
+++ app/controllers/hero_controller.rb
@@ -3,4 +3,19 @@ class HeroController < ApplicationController
         heros = Hero.all
         heros.to_json
     end
+
+    get '/heros/mage' do
+        mageHeros = Hero.find_by_name("Mage").cards
+        mageHeros.to_json
+    end
+
+    get '/heros/druid' do
+        mageHeros = Hero.find_by_name("Druid").cards
+        mageHeros.to_json
+    end
+
+    get '/heros/warrior' do
+        mageHeros = Hero.find_by_name("warrior").cards
+        mageHeros.to_json
+    end
 end
